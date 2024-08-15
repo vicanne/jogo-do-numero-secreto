73e957c39a20b6c50265cbc50f749b1db0038371
vicanne <vicctoria.anne@gmail.com>
2024-08-15 16:52:49 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,7 @@ responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 function exibirMensagemInicial() {
 
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 exibirMensagemInicial();
 
