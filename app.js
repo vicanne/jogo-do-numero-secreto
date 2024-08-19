6d40e3102f9f8ed769aeca33ee6dcb07dca77384
vicanne <vicctoria.anne@gmail.com>
2024-08-19 19:30:18 -0300

    Deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
